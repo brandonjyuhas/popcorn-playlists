02aaa30b3811637f9eaf08faec3b6e1df50e42ec
Brandon Yuhas <brandonjyuhas@gmail.com>
2014-12-11 22:17:33 -0500

    Create Sign in Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
-  resources :welcome_pages, only: [:index]
+  root        'welcome_pages#index'
+  devise_for  :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+  resources   :welcome_pages, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
